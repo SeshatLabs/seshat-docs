abdbf1bf78f40bfcc879ef27861a555e82064e05
hosseinkhatiri <hkhk.sharif@gmail.com>
2023-04-03 14:03:35 -0400

    ignore brokenlink

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: 'SeshatLabs', // Usually your GitHub org/user name.
   projectName: 'Seshat-docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
